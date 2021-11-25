cdc03ddafdd4ea5f3798535808c4df5a0a142d3e
fabii <fabii@DESKTOP-N7HH15T>
2021-11-25 09:56:16 +0100

    Agregar archivos de proyecto.

---
+++ EjerciciosCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EjerciciosCSharp
+{
+
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, Fabio!");
+
+            // Ejercicio 3.
+
+        }
+    }
+}
+
